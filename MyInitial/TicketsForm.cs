249289f0bd5070944d7eb1f4f8dd628d2a565285
Luke Mills <lsm.web.contact@gmail.com>
2018-08-20 13:54:20 +0930

    Initial loading of MyInitial

---
+++ MyInitial/TicketsForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ticketing
+{
+    public partial class TicketsForm : Form
+    {
+        TicketPrice mTicketPrice;
+        int mSection = 2;
+        int mQuantity = 0;
+        bool mDiscount = false;
+
+        public TicketsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsForm_Load(object sender, EventArgs e)
+        {
+            txtBxDiscount.Text = "5.0";
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            mQuantity = int.Parse(txtQuantity.Text);
+
+            if (chkDiscount.Checked)
+                { mDiscount = true; }
+
+            if (radBalcony.Checked)
+                { mSection = 1; }
+            if (radGeneral.Checked)
+                { mSection = 2; }
+            if (radBox.Checked)
+                { mSection = 3; }
+
+            mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
+
+            mTicketPrice.calculatePrice();
+            lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
+        }
+
+        private void radGeneral_Click(object sender, EventArgs e)
+        {
+            txtBxDiscount.Text = "7.0";
+        }
+
+        private void radBalcony_Click(object sender, EventArgs e)
+        {
+            txtBxDiscount.Text = "6.0";
+        }
+
+        private void radBox_Click(object sender, EventArgs e)
+        {
+            txtBxDiscount.Text = "8.0";
+        }
+    }
+}
